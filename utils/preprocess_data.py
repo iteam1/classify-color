2d6780a2903532388bda9495d39c4a13a05b6bdb
locchuong123 <locchuong123@gmail.com>
2023-07-23 16:28:13 +0700

    update

--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -11,7 +11,7 @@ DIM = 256
 
 if not os.path.exists(OUTPUT):
     os.mkdir(OUTPUT)
-    
+
 colors = os.listdir(DATA_PATH)
 
 def preprocess(im):
@@ -19,15 +19,15 @@ def preprocess(im):
     im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
     H,W,C = im.shape
     mask = np.all(im != [0,0,0],axis=2)
-    
+
     # collect nonzero pixels
     for i in range(H):
         for j in range(W):
             if mask[i,j]:
                 pixels.append(im[i,j])
-    
+
     print(' total pixels:',H*W,'nonzero pixels:',len(pixels))
-    
+
     # convert to np.float32
     Z = np.float32(np.array(pixels))
     # define criteria, number of clusters and apply kmeans
@@ -35,7 +35,7 @@ def preprocess(im):
     # kmeans
     ret, labels, centroids = cv2.kmeans(Z, NUM_CLUSTER, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
     centroids = np.uint8(centroids)
-    
+
     dict = []
     l = []
     vals = list(set(list(labels.flatten())))
@@ -46,7 +46,7 @@ def preprocess(im):
         l.append(qua_DIM)
         dict.append({'color':centroids[val],'density':des,'quantity':qua,f'quantity_{DIM}':qua_DIM})
         # print(' color:',centroids[val],' density:',des,' quantity:',qua,f' quantity_{DIM}:',qua_DIM)
-    
+
     # convert to box
     box = []
     for m in range(NUM_CLUSTER - 1):
@@ -58,33 +58,30 @@ def preprocess(im):
     current_color = dict[-1]['color']
     current_box = [current_color]*(DIM*DIM - len(box))
     box += current_box
-    
+
     box = np.array(box).reshape((DIM,DIM,3))
-    
+
     return box
 
 for color in colors:
-    
+
     # create color folder
     if not os.path.exists(os.path.join(OUTPUT,color)):
         os.mkdir(os.path.join(OUTPUT,color))
-    
+
     imgs = os.listdir(os.path.join(DATA_PATH,color))
 
     for idx,img in enumerate(imgs):
-        
+
         # init
         PATH = os.path.join(DATA_PATH,color,img)
-        
+
         print(idx,' Processing:',PATH)
         pixels = []
-        
+
         # collect pixels
         im = cv2.imread(PATH)
-        
+
         box = preprocess(im)
-        
+
         cv2.imwrite(os.path.join(OUTPUT,color,img),cv2.cvtColor(box,cv2.COLOR_RGB2BGR))
-        
-    
-    
