78d1c332696e08de8a8ff80d0fefd33c8db90f59
locchuong123 <locchuong123@gmail.com>
2023-07-23 20:33:22 +0700

    update;

--- visualize_data.py
+++ visualize_data.py
@@ -1,5 +1,5 @@
 '''
-python3 visualize_data.py data/COLOR2
+python3 visualize_data.py data/COLOR6
 '''
 import os
 import sys
@@ -12,40 +12,61 @@ DATA_PATH = sys.argv[1] # image path
 NUM_CLUSTER = 3 # number of cluster
 MAX_VALUE = 255
 
-colors = ['blue'] #os.listdir(DATA_PATH)
+colors = os.listdir(DATA_PATH)
 
 dataset = {}
 
-R = []
-G = []
-B = []
+RGBs = []
 
 # collect data
 for color in colors:
+
+    print('Parsing ',color)
+
+    # list all image
     imgs = os.listdir(os.path.join(DATA_PATH,color.upper()))
 
-    for img_name in imgs:
+    # init
+    R = []
+    G = []
+    B = []
+
+    for n,img_name in enumerate(imgs):
+
         # init
         pixels = []
+
         # read the image
         img = cv2.imread(os.path.join(DATA_PATH,color.upper(),img_name))
         h,w,c = img.shape
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        #img = cv2.cvtColor(img,cv2.COLOR_BGR2LAB)
+
         # remove zeropixel
         mask = np.all(img != [0,0,0],axis = 2)
         for i in range(h):
             for j in range(w):
                 if mask[i,j]:
                     pixels.append(img[i,j])
-        print(img_name,'total pixels:',h*w,' zeropixels:',h*w-len(pixels))
+
+        total_pixels = h*w
+        zero_pixels = h*w-len(pixels)
+
+        print(n,img_name,'total pixels:',total_pixels,' zero_pixels:',zero_pixels,\
+        round((total_pixels-zero_pixels)/total_pixels,2))
+
         # convert to numpy array
         Z = np.array(pixels)
+
         # convert to np.float32
         Z = np.float32(Z)
+
         # define criteria, number of clusters and apply kmeans
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
         # kmeans
-        ret, labels, centers = cv2.kmeans(Z, NUM_CLUSTER, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        ret, labels, centers = cv2.kmeans(Z, NUM_CLUSTER, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
         centers = centers.astype(np.uint8)
 
         # collect R,G,B value
@@ -55,18 +76,23 @@ for color in colors:
             G.append(current_color[1])
             B.append(current_color[2])
 
+    RGBs.append([R,G,B])
+
 # visualize data
 fig = plt.figure(figsize=(10,7))
 ax = plt.axes(projection='3d')
 
-# creating plot
-ax.scatter3D(R,G,B,color='blue')
-plt.title('simple 3D scatter plot')
-ax.set_xlim(0,MAX_VALUE)
-ax.set_ylim(0,MAX_VALUE)
-ax.set_xlabel('R axis')
-ax.set_ylabel('G axis')
-ax.set_zlabel('B axis')
+for i in range(len(colors)):
+    R,G,B = RGBs[i]
+
+    # creating plot
+    ax.scatter3D(R,G,B,color=colors[i])
+    plt.title('HSV 3D scatter plot')
+    ax.set_xlim(0,MAX_VALUE)
+    ax.set_ylim(0,MAX_VALUE)
+    ax.set_xlabel('R axis')
+    ax.set_ylabel('G axis')
+    ax.set_zlabel('B axis')
 
 # show plot
 plt.show()
